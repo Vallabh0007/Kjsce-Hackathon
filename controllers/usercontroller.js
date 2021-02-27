37bfbeea99ce1c65eeb146ce2d785e6051ff8beb
Vallabh0007 <67462145+Vallabh0007@users.noreply.github.com>
2021-02-27 15:20:57 +0530

    Home
    added home page

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 let connection = require('../config');
 module.exports.register=function(req,res){
     let users={
@@ -17,4 +18,25 @@ module.exports.register=function(req,res){
         })
       }
     });
+=======
+let connection = require('../config');
+module.exports.register=function(req,res){
+    let users={
+        "username":req.body.username,
+        "password":req.body.password,
+        "email":req.body.email,
+    }
+    connection.query('INSERT INTO users SET ?',users, function (error, results, fields) {
+      if (error) {
+        res.json({
+            message:'there are some error with query'
+        })
+      }else{
+          res.json({
+            data:results,
+            message:'user registered sucessfully'
+        })
+      }
+    });
+>>>>>>> Stashed changes
 }
