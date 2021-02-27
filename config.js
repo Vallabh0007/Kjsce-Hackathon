0b8f10d5b20274ea799581f94431b866237a19a9
sahil-777 <turbothunderboltz@gmail.com>
2021-02-28 01:25:40 +0530

    Updated: homepage

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "password",
+  password: "Pass@1234",
   debug: false,
   multipleStatements: true
 });
