f8f577d1ec6b1a0b48e3b03ac96249a8c472bd00
Pratik Zinjurde <zinjurdepratik.pz@gmail.com>
2021-02-27 19:07:56 +0530

    login controller added

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -1,5 +1,5 @@
 let connection = require('../config');
-module.exports.register=function(req,res){
+const register=function(req,res){
     let users={
         "username":req.body.username,
         "password":req.body.password,
@@ -7,14 +7,40 @@ module.exports.register=function(req,res){
     }
     connection.query('INSERT INTO users SET ?',users, function (error, results, fields) {
       if (error) {
+        console.log(error)
         res.json({
             message:'Error with signup query'
         })
       }else{
-          res.json({
-            data:results,
-            message:'user registered sucessfully'
-        })
+        // console.log(results);
+        // res.json({ results })
+        res.redirect('/api/login');
       }
     });
+}
+
+const login = function(req,res){
+  var username = req.body.username;
+  var password = req.body.password;
+  if (username && password) {
+      connection.query('SELECT * FROM users WHERE username = ?', username, function(error, results, fields) {
+          if (results.length > 0) {
+              req.session.loggedin = true;
+              req.session.username = username;
+              // res.json({ results })
+              res.redirect('/home');
+          } else {
+              res.send('Incorrect Username and/or Password!');
+          }           
+          res.end();
+      });
+    } else {
+        res.send('Please enter Username and Password!');
+        res.end();
+    }
+}
+
+module.exports = { 
+	register,
+	login
 }
