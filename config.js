25d6fad32dd367895cd488a0eef41e537d1461fa
Pratik Zinjurde <zinjurdepratik.pz@gmail.com>
2021-02-27 14:04:13 +0530

    added

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "Pass@1234",
+  password: "password",
   debug: false,
   multipleStatements: true
 });
