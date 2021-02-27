3113aea1f0f523ce8f3b8be012d1cda8302c9781
sahil-777 <turbothunderboltz@gmail.com>
2021-02-27 16:05:01 +0530

    Updated: Change style path

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 let connection = require('../config');
 module.exports.register=function(req,res){
     let users={
@@ -18,25 +17,4 @@ module.exports.register=function(req,res){
         })
       }
     });
-=======
-let connection = require('../config');
-module.exports.register=function(req,res){
-    let users={
-        "username":req.body.username,
-        "password":req.body.password,
-        "email":req.body.email,
-    }
-    connection.query('INSERT INTO users SET ?',users, function (error, results, fields) {
-      if (error) {
-        res.json({
-            message:'there are some error with query'
-        })
-      }else{
-          res.json({
-            data:results,
-            message:'user registered sucessfully'
-        })
-      }
-    });
->>>>>>> Stashed changes
 }
